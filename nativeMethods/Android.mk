7a4d284e5dafb831cb0ced9fc4d1702e70166fe0
Transtech Build <devops@transtech.net.au>
2016-06-17 16:46:38 +1000

    Test updated flinger SO

--- nativeMethods/Android.mk
+++ nativeMethods/Android.mk
@@ -1,3 +1,5 @@
 # The namespace in Java file, with dots replaced with underscores
+LOCAL_PATH := $(call my-dir)
+
 include $(call all-subdir-makefiles)
  
--- nativeMethods/flinger/flinger.cpp
+++ nativeMethods/flinger/flinger.cpp
@@ -17,72 +17,179 @@
 	 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 	 */
 
+#ifdef __ANDROID__
+# include <android/api-level.h>
+#endif
+
 #include "flinger.h"
 #include "screenFormat.h"
 
-#include <binder/IPCThreadState.h>
-#include <binder/ProcessState.h>
-#include <binder/IServiceManager.h>
+#ifdef __ANDROID_API__
 
-#include <binder/IMemory.h>
-#include <surfaceflinger/ISurfaceComposer.h>
-#include <surfaceflinger/SurfaceComposerClient.h>
+# if __ANDROID_API__ > 15
+#  include <binder/IPCThreadState.h>
+#  include <binder/ProcessState.h>
+#  include <gui/SurfaceComposerClient.h>
+#  include <gui/ISurfaceComposer.h>
+#  include <ui/DisplayInfo.h>
+#  include <ui/PixelFormat.h>
 
 using namespace android;
 
-ScreenshotClient *screenshotClient=NULL;
+static uint32_t DEFAULT_DISPLAY_ID = ISurfaceComposer::eDisplayIdMain;
+// Maps orientations from DisplayInfo to ISurfaceComposer
+static const uint32_t ORIENTATION_MAP[] = {
+    ISurfaceComposer::eRotateNone, // 0 == DISPLAY_ORIENTATION_0
+    ISurfaceComposer::eRotate270, // 1 == DISPLAY_ORIENTATION_90
+    ISurfaceComposer::eRotate180, // 2 == DISPLAY_ORIENTATION_180
+    ISurfaceComposer::eRotate90, // 3 == DISPLAY_ORIENTATION_270
+};
+static sp<IBinder> display;
+uint32_t captureOrientation = ISurfaceComposer::eRotateNone;
+# else
+#  include <binder/IPCThreadState.h>
+#  include <binder/ProcessState.h>
+#  include <binder/IServiceManager.h>
+#  include <binder/IMemory.h>
+#  include <surfaceflinger/ISurfaceComposer.h>
+#  include <surfaceflinger/SurfaceComposerClient.h>
+
+using namespace android;
+
+# endif //__ANDROID_API__ > 6
+#endif //__ANDROID_API__
+
+static ScreenshotClient * screenshotClient = NULL;
+
+#if __ANDROID_API__ > 15
 
 extern "C" screenFormat getscreenformat_flinger()
 {
-  //get format on PixelFormat struct
-	PixelFormat f=screenshotClient->getFormat();
+  screenFormat format;
+  uint32_t f;
 
-	PixelFormatInfo pf;
-	getPixelFormatInfo(f,&pf);
-
-	screenFormat format;
-
-	format.bitsPerPixel = pf.bitsPerPixel;
-	format.width = screenshotClient->getWidth();
-	format.height =     screenshotClient->getHeight();
-	format.size = pf.bitsPerPixel*format.width*format.height/CHAR_BIT;
-	format.redShift = pf.l_red;
-	format.redMax = pf.h_red;
-	format.greenShift = pf.l_green;
-	format.greenMax = pf.h_green-pf.h_red;
-	format.blueShift = pf.l_blue;
-	format.blueMax = pf.h_blue-pf.h_green;
-	format.alphaShift = pf.l_alpha;
-	format.alphaMax = pf.h_alpha-pf.h_blue;
-
-	return format;
+  format.width  = screenshotClient->getWidth();
+  format.height = screenshotClient->getHeight();
+  format.size = screenshotClient->getSize();
+  f = screenshotClient->getFormat();
+  format.bitsPerPixel = android::bitsPerPixel(f);
+  if( f == android::PIXEL_FORMAT_RGB_565 )
+  {
+    format.redShift = 11;
+    format.redMax = 16;
+    format.greenShift = 5;
+    format.greenMax = 10;
+    format.blueShift = 0;
+    format.blueMax = 4;
+    format.alphaShift = 0;
+    format.alphaMax = 0;
+  }
+  else
+  {
+    format.redShift = 24;
+    format.redMax = 32;
+    format.greenShift = 16;
+    format.greenMax = 23;
+    format.blueShift = 8;
+    format.blueMax = 15;
+    format.alphaShift = 0;
+    format.alphaMax = 7;
+    
+  }
+  return format;
 }
-
 
 extern "C" int init_flinger()
 {
-	int errno;
-
-	L("--Initializing gingerbread access method--\n");
+  L("--Initializing jellybean access method--\n");
 
   screenshotClient = new ScreenshotClient();
-	errno = screenshotClient->update();
+
+  display = SurfaceComposerClient::getBuiltInDisplay(DEFAULT_DISPLAY_ID);
+  if (display == NULL) {
+        L( "Unable to get handle for display\n");
+        return -1;
+  }
+  
+  Vector<DisplayInfo> configs;
+  SurfaceComposerClient::getDisplayConfigs(display, &configs);
+  int activeConfig = SurfaceComposerClient::getActiveConfig(display);
+  if (static_cast<size_t>(activeConfig) >= configs.size()) {
+      L("Active config not inside configs\n");
+      return -1;
+  }
+  
+  uint8_t displayOrientation = configs[activeConfig].orientation;
+  uint32_t captureOrientation = ORIENTATION_MAP[displayOrientation];
+  status_t result = screenshotClient->update(display, Rect(), 0, 0, 0, -1U, false, captureOrientation);
   if (!screenshotClient->getPixels())
     return -1;
 
-  if (errno != NO_ERROR) {
-		return -1;
-	}
-	return 0;
+  if (result != NO_ERROR)
+    return -1;
+
+  return 0;
 }
 
 extern "C" unsigned int *readfb_flinger()
 {
-	screenshotClient->update();
-	return (unsigned int*)screenshotClient->getPixels();
+  status_t result = screenshotClient->update(display, Rect(), 0, 0, 0, -1U, false, captureOrientation);
+  return (unsigned int*)screenshotClient->getPixels();
 }
+
+#else
+
+extern "C" screenFormat getscreenformat_flinger()
+{
+  //get format on PixelFormat struct
+  PixelFormat f = screenshotClient->getFormat();
+
+  PixelFormatInfo pf;
+  getPixelFormatInfo(f,&pf);
+
+  screenFormat format;
+  format.bitsPerPixel = pf.bitsPerPixel;
+  format.width = screenshotClient->getWidth();
+  format.height =     screenshotClient->getHeight();
+  format.size = pf.bitsPerPixel*format.width*format.height/CHAR_BIT;
+  format.redShift = pf.l_red;
+  format.redMax = pf.h_red;
+  format.greenShift = pf.l_green;
+  format.greenMax = pf.h_green-pf.h_red;
+  format.blueShift = pf.l_blue;
+  format.blueMax = pf.h_blue-pf.h_green;
+  format.alphaShift = pf.l_alpha;
+  format.alphaMax = pf.h_alpha-pf.h_blue;
+
+  return format;
+}
+
+extern "C" int init_flinger()
+{
+  int errno;
+
+  L("--Initializing gingerbread access method--\n");
+
+  screenshotClient = new ScreenshotClient();
+  errno = screenshotClient->update();
+  if (!screenshotClient->getPixels())
+    return -1;
+
+  if (errno != NO_ERROR)
+    return -1;
+
+  return 0;
+}
+
+extern "C" unsigned int *readfb_flinger()
+{
+  screenshotClient->update();
+  return (unsigned int*)screenshotClient->getPixels();
+}
+#endif //__ANDROID_API__ > 16
 
 extern "C" void close_flinger()
 {
   free(screenshotClient);
 }
+
